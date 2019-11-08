98405a7894dd63f4fb580438f3ab722c63ea1350
Akash Srivastava <akash.srivastava@bigbinary.com>
2019-11-08 14:09:43 +0530

    Building navbar

--- app/index.js
+++ app/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css"
 
+import Popular from './components/Popular'
+
 class App extends React.Component {
+
+    
     render(){
+        
         return (
-            <div>
-                Hello World!
+            <div className='container'>
+                <Popular/>
             </div>
         )
     }
