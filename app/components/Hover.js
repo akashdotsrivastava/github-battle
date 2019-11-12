e85c79c0855a97b26cb595f9b7fb82d213576a28
Akash Srivastava <akash.srivastava@bigbinary.com>
2019-11-12 15:25:37 +0530

    Refactoring using class fields

--- app/components/Hover.js
+++ app/components/Hover.js
@@ -3,22 +3,17 @@ import PropTypes from 'prop-types';
 import { throws } from 'assert';
 
 export default class Hover extends React.Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            hovering: false,
-        }
-    
-        this.mouseOver = this.mouseOver.bind(this)
-        this.mouseOut = this.mouseOut.bind(this)
+    state = {
+        hovering: false
     }
-    mouseOver() {
+
+    
+    mouseOver = () => {
         this.setState({
             hovering: true
         })
     }
-    mouseOut() {
+    mouseOut = () => {
         this.setState({
             hovering: false
         })
